6dc2b60227211befbeebddcc3ef792d486ee4b3b
subher0 <subher0@mail.ru>
2020-07-08 21:42:51 +0300

    initial commit

---
+++ language-tutor-back/src/main/kotlin/org/subher0/language/tutor/LanguageTutorApplication.kt
@@ -0,0 +1,11 @@
+package org.subher0.language.tutor
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LanguageTutorApplication
+
+fun main(args: Array<String>) {
+	runApplication<LanguageTutorApplication>(*args)
+}
---
+++ language-tutor-back/src/test/kotlin/org/subher0/language/tutor/LanguageTutorApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.subher0.language.tutor
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LanguageTutorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
